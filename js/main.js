5ca6f3961c4f334d06a2fd40094f9e55acc4fcba
Adam Christopher Backlas Widjaja <adam.keristoper@gmail.com>
2017-04-27 10:22:29 +0900

    Upload to github

--- js/main.js
+++ js/main.js
@@ -3,11 +3,10 @@ function LazyLoad(imgString) {
   var images = Array.prototype.slice.call(document.querySelectorAll(imgString));
   var imgsrc = images.map(function(image) {
     var cloned = image.cloneNode(image);
-    image.src = "img/kit-kat.jpg";
+    image.src = "img/kit-kat.jpg";//temporary image(or gif)
     cloned.addEventListener("load", function(e) {
       image.src = e.target.src;
-      image.classList.add("active");
-      console.log(image);
+      image.classList.add("active");//class to trigger opacity animation
     }, false);
   });
 }
